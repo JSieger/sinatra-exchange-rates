5498c8d20b35dde818f859621c53433dc86ed0b2
Joseph Sieger <jsieger44@gmail.com>
2023-12-17 20:05:18 +0000

    changed the name to the way we named api key

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require "http"
 get("/") do
 
   # build the API url, including the API key in the query string
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_KEY"]}"
 
   # use HTTP.get to retrieve the API information
   raw_data = HTTP.get(api_url)
