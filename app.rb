abdebd36c0bfe1d0ae8b5506b7c7a00cf36aa6f4
Joseph Sieger <jsieger44@gmail.com>
2023-12-26 17:36:13 +0000

    dynamic link and currency page

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
-# define a route
+
 get("/") do
 
   # build the API url, including the API key in the query string
@@ -22,4 +22,26 @@ get("/") do
 
   # render a view template where I show the symbols
   erb(:homepage)
+end
+
+get("/:currency") do
+  # build the API url, including the API key in the query string
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = parsed_data["currencies"]
+
+  @currency = params.fetch("currency")
+
+  erb(:currency)
+
 end
